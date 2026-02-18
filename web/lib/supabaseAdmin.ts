aaace938f28bacb40826eeaa7ff9021bdd1760eb
anderson06024-gif <anderson06024@gmail.com>
2026-02-19 06:47:35 +0800

    Update supabaseAdmin.ts

--- web/lib/supabaseAdmin.ts
+++ web/lib/supabaseAdmin.ts
@@ -2,8 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 import { mustGetEnv } from "./env";
 
 export function supabaseAdmin() {
-  const url = mustGetEnv("NEXT_PUBLIC_SUPABASE_URL");
-  const serviceKey = mustGetEnv("SUPABASE_SERVICE_ROLE_KEY");
+  const url = mustGetEnv("https://iaqkypswuadhmrxnseig.supabase.co
+");
+  const serviceKey = mustGetEnv("https://iaqkypswuadhmrxnseig.supabase.co/auth/v1
+");
   return createClient(url, serviceKey, {
     auth: { persistSession: false, autoRefreshToken: false },
   });
